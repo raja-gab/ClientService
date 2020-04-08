1d19c35b60bc752b80a938bfc796a746ea485ac4
omarkorbi <46576861+omarkorbi@users.noreply.github.com>
2020-04-08 22:59:36 +0200

    Update Article.java

--- src/main/java/com/example/demo/entity/Article.java
+++ src/main/java/com/example/demo/entity/Article.java
@@ -1,1 +1,23 @@
 
+package com.example.demo.entity;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@Data
+@AllArgsConstructor @NoArgsConstructor @ToString
+public class Article {
+  
+	private String idArt;
+	private String designtationArt;
+	
+	private Avis avisArt ; 
+
+	
+	
+}
