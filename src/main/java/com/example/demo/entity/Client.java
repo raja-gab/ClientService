d6f6e5ada7eca40e791a1eb2386b7fb78d7214da
omarkorbi <46576861+omarkorbi@users.noreply.github.com>
2020-04-08 23:00:21 +0200

    Create Client.java

---
+++ src/main/java/com/example/demo/entity/Client.java
@@ -0,0 +1,17 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+
+	private String cinCli; 
+	
+	private String adresseCli;
+	private String numTelCli;
+	
+}
