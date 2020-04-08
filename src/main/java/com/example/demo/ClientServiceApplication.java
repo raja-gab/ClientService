70ab361d697f04c9d2a806bc6670f0ae1cc78fdb
omarkorbi <46576861+omarkorbi@users.noreply.github.com>
2020-04-08 23:03:25 +0200

    Update ClientServiceApplication.java

--- src/main/java/com/example/demo/ClientServiceApplication.java
+++ src/main/java/com/example/demo/ClientServiceApplication.java
@@ -2,10 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
 public class ClientServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ClientServiceApplication.class, args);
 	}
