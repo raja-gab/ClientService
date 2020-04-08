cb4f2758580a4d3d4d37fe5aa339f5c6440f8a4f
omarkorbi <46576861+omarkorbi@users.noreply.github.com>
2020-04-08 22:58:08 +0200

    Create ClientController.java

---
+++ src/main/java/com/example/demo/controller/ClientController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entity.Avis;
+import com.example.demo.entity.Reclamation;
+import com.example.demo.service.CrudRestClient;
+
+@RestController
+public class ClientController {
+
+	@Autowired
+	private CrudRestClient crudRest;
+	
+	
+	@PostMapping("/avis")
+	public Avis avis( @RequestBody Avis avis) {
+		Avis avs = new Avis();
+		avs.setAvis(avis.getAvis());
+		avs.setDateAvis(java.time.LocalDate.now());
+		avs.setClient(avis.getClient());
+		
+		crudRest.postAvis(avs);
+		return avs ;
+
+	}
+	
+	@PostMapping("/reclamation")
+	public Reclamation reclamation(@RequestBody Reclamation reclamation) {
+		Reclamation rec = new Reclamation();
+		rec.setDateRec(java.time.LocalDate.now());
+		rec.setReclamation(reclamation.getReclamation());
+		rec.setCommande(reclamation.getCommande());
+		
+		crudRest.postReclamation(rec);
+		return rec ;
+	}
+	
+}
