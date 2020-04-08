526d09fd1b80848607cab6cb0783082eb72312f5
omarkorbi <46576861+omarkorbi@users.noreply.github.com>
2020-04-08 23:03:06 +0200

    Create CrudRestService.java

---
+++ src/main/java/com/example/demo/service/CrudRestService.java
@@ -0,0 +1,28 @@
+package com.example.demo.service;
+
+import java.util.Optional;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.demo.entity.Article;
+import com.example.demo.entity.Avis;
+import com.example.demo.entity.Reclamation;
+
+@FeignClient(name = "CrudApplication" , url = "http://localhost:8011/CrudApplication")
+public interface CrudRestClient {
+	
+	@GetMapping("/getproduct/{id}")
+	public Optional<Article> getArticleById();
+	
+	@PostMapping("/avis")
+	public Optional<Avis> postAvis( @RequestBody Avis avis);
+	
+	@PostMapping("/reclamation")
+	public Optional<Avis> postReclamation( @RequestBody Reclamation reclamation);
+	
+	
+
+}
