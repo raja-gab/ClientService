ba64992f7fd8c5ffe7caeba25c8b6602aa45a423
omarkorbi <46576861+omarkorbi@users.noreply.github.com>
2020-04-08 23:00:04 +0200

    Create Avis.java

---
+++ src/main/java/com/example/demo/entity/Avis.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Avis {
+	private String idAvis ;
+	private String avis ; 
+	private LocalDate dateAvis ; 
+	
+	private Client client;
+	
+}
